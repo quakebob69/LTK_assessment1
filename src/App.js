fc5120731deea3241653d051bcb3f6e3bef5a34d
quakebob69 <Trad3rd@n>
2023-10-01 16:03:12 -0600

    router package, home and todo page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Link as RouterLink, Route, Routes } from "react-router-dom";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import AccountMenu from './AccountMenu';
 import BasicForm from './BasicForm'
+import Home from './pages/home';
+import Todo from './pages/todo';
 
 export default function App() {
 	const [openForm, setOpenForm] = React.useState(false);
 	const handleOnClick = (page) => {
 		console.log('open');
-		if(!openForm) { 
+		if (!openForm) {
 			setOpenForm(true)
 		} else {
 			setOpenForm(false)
 		}
 	};
-  return (
-    <Container maxWidth="sm">
-	<AccountMenu onClick={handleOnClick} />
-	{openForm && <BasicForm />}
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          React Skills test
-        </Typography>
-      </Box>
-    </Container>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/todo" element={<Todo />} />
+			</Routes>
+		</Router>
+	);
 }
