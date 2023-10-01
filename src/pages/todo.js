7ee787b2594b7bdd893d545c210715474a88b3a0
quakebob69 <Trad3rd@n>
2023-10-01 16:43:24 -0600

    added material ui table to todo page

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,10 +1,12 @@
 import BasicForm from './../components/BasicForm'
+import BasicTable from './../components/BasicTable'
 
 export default function Todo() {
     return (
         <div className="todo">
             <h2>Todo List</h2>
                 <BasicForm />
+                <BasicTable />
         </div>
     )
 }
