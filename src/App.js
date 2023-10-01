449b96251e5aaa692bfcea28821960c0e11dc878
quakebob69 <Trad3rd@n>
2023-10-01 15:30:19 -0600

    basicform component wrong name, added formik package, added materialui package

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <Container maxWidth="sm">
 	<AccountMenu onClick={handleOnClick} />
-	{openForm && <Basicorm />}
+	{openForm && <BasicForm />}
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           React Skills test
