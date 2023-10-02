12d2813b8a4213b3984589da9579112a32d60736
quakebob69 <Trad3rd@n>
2023-10-01 18:34:04 -0600

    simpled grid layout for site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { BrowserRouter as Router, NavLink, Route, Routes } from "react-router-dom";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import AccountMenu from './components/AccountMenu';
@@ -19,28 +20,18 @@ export default function App() {
 		}
 	};
 	return (
-
-
-
-		<Router>
-			<Container maxWidth="sm">
-				<Box sx={{ my: 4 }}>
-					<NavLink to="/">
-						<Typography variant="h4" component="h1" gutterBottom>
-							React Skills test
-						</Typography>
-					</NavLink>
-				</Box>
-				<AccountMenu onClick={handleOnClick} />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/todo" element={<Todo />} />
-				</Routes>
-			</Container>
-		</Router>
-
-
-
-
+		<Grid container spacing={2}>
+			<Grid item xs={8}>
+				title
+			</Grid>
+			<Grid item xs={4}>
+				menu
+			</Grid>
+			<Grid item xs={2} />
+			<Grid item xs={8}>
+				todo stuff
+			</Grid>
+			<Grid item xs={2} />
+		</Grid>
 	);
 }
